6992c2e70751f3d29b9082438ed3662dec7a117d
gautam <gautambk98@gmail.com>
2020-06-02 17:52:48 +0530

    Made it easier to change number of points generated

--- test/makePoints.py
+++ test/makePoints.py
@@ -5,12 +5,14 @@ import numpy as np
 pi = np.pi
 
 with open("sphericalPointCloud.txt", "w") as f:
-    for i in range(51):
-        phi = i * pi / 50
+    ALPHA = 25
+    BETA = 9
+    for i in range(ALPHA + 1):
+        phi = i * pi / ALPHA
         r = radius * np.sin(phi)
         z = radius * np.cos(phi)
-        for j in range(int(np.ceil(r * 12))):
-            theta = j * pi / 6 + np.random.uniform(0, pi)
+        for j in range(int(np.ceil(r * BETA))):
+            theta = 2 * j * pi / BETA + np.random.uniform(0, pi)
             x = r * np.cos(theta)
             y = r * np.sin(theta)
             f.write("%f %f %f %f %f %f\n" % (x, y, z, x, y, z))
